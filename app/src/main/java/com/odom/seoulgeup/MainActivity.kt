caa793209fe271520a6e42e5eb14b4fb4770a353
unknown <jihoon708@naver.com>
2020-10-15 22:02:08 +0900

    14th -4 Google Maps

--- app/src/main/java/com/odom/seoulgeup/MainActivity.kt
+++ app/src/main/java/com/odom/seoulgeup/MainActivity.kt
@@ -30,7 +30,6 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.Circle
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.clustering.ClusterManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -38,47 +37,7 @@ import kotlinx.android.synthetic.main.search_bar.view.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
-import kotlin.math.pow
 import kotlin.system.exitProcess
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.MarkerOptions
-
-internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(
-            MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-}
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -92,9 +51,6 @@ class MainActivity : AppCompatActivity() {
     val DEFAULT_ZOOM_LEVEL = 18f
     val CITY_HALL = LatLng(37.566648, 126.978449)
     var googleMap: GoogleMap? = null
-
-    val myLoc = Location(LocationManager.NETWORK_PROVIDER)
-    val targetLoc = Location(LocationManager.NETWORK_PROVIDER)
 
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
